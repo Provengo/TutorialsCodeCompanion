f0ad0a29c0d6df169f3a58c067d40b87b05aebe5
Michael Bar-Sinai <mich.barsinai@gmail.com>
2024-03-04 11:48:50 +0200

    added starting point

---
+++ 3-creating-test-suites/start/lib/Actions.js
@@ -0,0 +1,29 @@
+const Actions = (function () {
+
+  const LIB_SIG = "ACTIONS";
+
+  function makeActionEvent(verb, complement) {
+    return Event(verb + " " + complement, {
+      verb: verb,
+      complement: complement,
+      lib: LIB_SIG
+    });
+  }
+
+  return {
+    wakeUp:     makeActionEvent("wake up", ""),
+    wearShirt:  makeActionEvent("wear", "shirt"),
+    wearPants:  makeActionEvent("wear", "pants"),
+    wearSocks:  makeActionEvent("wear", "socks"),
+    wearShoes:  makeActionEvent("wear", "shoes"),
+    eatCereal:  makeActionEvent("eat", "cereal"),
+    eatBanana:  makeActionEvent("eat", "banana"),
+    brushTeeth: makeActionEvent("brush teeth", ""),
+    tidyUp:     makeActionEvent("tidy up", ""),
+    goOut:      makeActionEvent("go out", ""),
+    
+    any: EventSet("Any Action", function (e) {
+      return (!!e.data) && ( e.data.lib === LIB_SIG );
+    }),
+  };
+})();
